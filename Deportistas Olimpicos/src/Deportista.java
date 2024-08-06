4acc40e36bcfa751ed5b7d5c6a6869a694abd339
Alejandra Durán <79487144+Alejandraqt@users.noreply.github.com>
2024-08-06 14:25:42 -0500

    Update Deportista
    Creacion metodo para mostrarDatos

--- Deportistas Olimpicos/src/Deportista.java
+++ Deportistas Olimpicos/src/Deportista.java
@@ -99,4 +99,9 @@ public class Deportista{
         this.puntuacion = puntuacion;
     }
 
+    //Cree esta clase para mostrar info pero toca ponerla mas bonita
+    public String mostrarDatos(){
+        return "Nombre: " + nombres + " Apellidos: " + apellidos + " Numero de identidad: " + numeroDeIdentidad + " Edad: "+ edad+" Deporte: " + deporte+" Sexo: " + sexo + " Pais: " + pais + " Medalla:"+ medalla + " Puntuicaoin: " +  puntuacion;
+    }
+    
 }
