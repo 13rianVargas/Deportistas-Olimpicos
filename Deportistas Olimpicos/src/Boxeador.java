e8e7ead32964b6bc2f92745f77c889eea9c69879
Alejandra Durán <79487144+Alejandraqt@users.noreply.github.com>
2024-08-06 14:22:47 -0500

    Actualizo Boxeador
    Creo todo

--- Deportistas Olimpicos/src/Boxeador.java
+++ Deportistas Olimpicos/src/Boxeador.java
@@ -1,3 +1,18 @@
 public class Boxeador extends Deportista {
+  private String tecnicaDeBoxeo;
 
+    public Boxeador(String nombres, String apellidos, int numeroDeIdentidad, int edad, String deporte, char sexo,
+            String pais, String medalla, double puntuacion, String tecnicaDeBoxeo) {
+        super(nombres, apellidos, numeroDeIdentidad, edad, deporte, sexo, pais, medalla, puntuacion);
+        this.tecnicaDeBoxeo = tecnicaDeBoxeo;
+    }
+
+    public String getTecnicaDeBoxeo() {
+        return tecnicaDeBoxeo;
+    }
+
+    @Override
+    public String mostrarDatos(){
+        return "Nombre: " + nombres + " Apellidos: " + apellidos + " Numero de identidad: " + numeroDeIdentidad + " Edad: "+ edad+" Deporte: " + deporte+" Sexo: " + sexo + " Pais: " + pais + " Medalla:"+ medalla + " Puntuicaoin: " +  puntuacion + "Tecnicva"+ tecnicaDeBoxeo;
+    }
 }
