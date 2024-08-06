7476fced8c68648b0a1ecf8ec7590d5cd621f041
Alejandra Durán <79487144+Alejandraqt@users.noreply.github.com>
2024-08-06 14:24:02 -0500

    Actualizo Tenista
    funxcionaa

--- Deportistas Olimpicos/src/Tenista.java
+++ Deportistas Olimpicos/src/Tenista.java
@@ -1,3 +1,17 @@
 public class Tenista extends Deportista {
+    private String estiloJuego;
 
+    public Tenista(String nombres, String apellidos, int numeroDeIdentidad, int edad, String deporte, char sexo, String pais, String medalla, double puntuacion, String estiloJuego) {
+        super(nombres, apellidos, numeroDeIdentidad, edad, deporte, sexo, pais, medalla, puntuacion);
+        this.estiloJuego = estiloJuego;
+    }
+
+    public String getEstiloJuego() {
+        return estiloJuego;
+    }
+
+    @Override
+    public String mostrarDatos(){
+        return "Nombre: " + nombres + " Apellidos: " + apellidos + " Numero de identidad: " + numeroDeIdentidad + " Edad: "+ edad+" Deporte: " + deporte+" Sexo: " + sexo + " Pais: " + pais + " Medalla:"+ medalla + " Puntuicaoin: " +  puntuacion + "Estilo de juego:"+ estiloJuego;
+    }
 }
